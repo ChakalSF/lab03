320a410755ae894c0c69490ae7d5b5e80120cdaf
SemenChakal <semachakal11@gmail.com>
2021-06-19 21:01:25 +0300

    2 commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 
-
+                                                        // Вариант 18 Чакал Семён А-03-20
 #include <iostream>
 
-#include <vector>                            // Вариант 18 Чакал Семён А-03-20
+#include <vector>
 
 #include <conio.h>
 
@@ -14,26 +14,85 @@ using namespace std;
 
 
 
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+svg_text(double left, double baseline, string text) {
+     cout << "<text x='" << left << "' y='"<< baseline << "' >" <<text << " </text>";
+}
+void svg_rect(double x, double y, double width, double height,string stroke, string fill){
+   cout<<" <rect x= '"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins,double max,const size_t MAX_ASTERISK) {
+    const auto IMAGE_WIDTH = 400;
+const auto IMAGE_HEIGHT = 300;
+const auto TEXT_LEFT = 20;
+const auto TEXT_BASELINE = 20;
+const auto TEXT_WIDTH = 50;
+const auto BIN_HEIGHT = 30;
+const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+      svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    //  svg_rect(TEXT_WIDTH,0,bins[0]*BLOCK_WIDTH,BIN_HEIGHT,"red","red");
+
+      double top = 0;
+      if (max<=MAX_ASTERISK)
+for (size_t bin : bins)
+
+    {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"green","black");
+    top = top+ BIN_HEIGHT;
+    }
+    else
+    for (size_t bin : bins)
+
+    {
+    const double bin_width = BLOCK_WIDTH * bin*MAX_ASTERISK/max;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"green","black");
+    top = top+ BIN_HEIGHT;
+    }
+
+
+    svg_end();
+}
 
 void show_histogramm_txt(vector <size_t>bins,double bin_Count,double high,const size_t MAX_ASTERISK ,char star)
 {
-    size_t maxCount=bins[0]; // Объявляем условно максимальное количество оценок в одном столбце гистрограммы
+    size_t maxCount=bins[0];
 
-    for(int i=0; i<bin_Count; i++)  //Находим этот максимум в цикле
-
+    for(int i=0; i<bin_Count; i++)
         if (bins[i]>maxCount)
 
             maxCount=bins[i];
 
-    if (maxCount>MAX_ASTERISK) // Находим коэффициент масштабирования ,если оно не требуется коэффициент остается равным 1
+    if (maxCount>MAX_ASTERISK)
 
         high=  static_cast<double>(MAX_ASTERISK)/(maxCount);
 
     else
 
         high=1;
-    size_t n=0;  //счетчик количества цифр в maxCount
-
+    size_t n=0;
     while(maxCount)
 
     {
@@ -61,7 +120,7 @@ void make_histogramm ( float bin_Size, double bin_Count, vector  <size_t> &bins,
 {
    bool flag;
 
-    for (int i = 0; i < numberCount; i++)     //Заполняем корзины количеством оценок,соответствующих данной корзине
+    for (int i = 0; i < numberCount; i++)
 
     {
 
@@ -128,7 +187,7 @@ int main()
 
     const size_t SCREEN_WIDTH = 80;
 
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1; //максимальное количество звездочек ,помещающихся в столбце гистограммы
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
     size_t numberCount,bin_Count;
 
@@ -142,21 +201,20 @@ int main()
 
     cerr << "numberCount=";
 
-    cin>>numberCount; //Вводим количество чисел(оценок)
+    cin>>numberCount;
 
 
-    const auto vec= input_numbers(numberCount);  //Объявляем  вектор для хранения оценок
-
+    const auto vec= input_numbers(numberCount);
   double min,max;
      find_minmax(min,max,vec,numberCount);
 
     cerr << "binCount=" << endl;
 
-    cin>>bin_Count; //Вводим количество корзин
+    cin>>bin_Count;
 
 
 
-    vector <size_t>bins(bin_Count); //Объявляем вектор для хранения оценок в корзинах
+    vector <size_t>bins(bin_Count);
 
     for (int i = 0; i < bin_Count; i++)
 
@@ -164,10 +222,10 @@ int main()
 
     bin_Size = (max - min) / bin_Count;
 
-    make_histogramm(bin_Size,bin_Count,bins,numberCount,min,max,vec); // Функция формирования гистограммы
+    make_histogramm(bin_Size,bin_Count,bins,numberCount,min,max,vec);
 
-   show_histogramm_txt(bins,bin_Count,high,MAX_ASTERISK,star);
 
+   show_histogram_svg(bins,max, MAX_ASTERISK);
 
 
     return 0;
